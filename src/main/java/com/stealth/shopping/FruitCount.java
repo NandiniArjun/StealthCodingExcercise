ba596bbcc2a319a3ec80d2bbfccab0e92cdf09ea
NandiniArjun <30292911+NandiniArjun@users.noreply.github.com>
2017-10-25 08:50:15 +0100

    Create FruitCount.java

---
+++ src/main/java/com/stealth/shopping/FruitCount.java
@@ -0,0 +1,23 @@
+package com.stealth.shopping;
+
+public class FruitCount {
+	
+	private int apples = 0, oranges = 0;
+	
+	void setApples(){
+		apples++;
+	}
+	
+	void setOranges(){
+		oranges++;
+	}
+	
+	int getApples(){
+		return apples;
+	}
+	
+	int getOranges(){
+		return oranges;
+	}
+
+}
